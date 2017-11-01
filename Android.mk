AospExtended/platform_packages_apps_Settings

Embed shortcut for root app on the dashboard
Launch SuperSU from Settings

* images taken from commit by Jorrit Jongma <git@jongma.org>

Change-Id: Ia5dafad75501fb9443e8d8b0470a10b51d0aa4d5

Add dashboard summary to SuperSU

Change-Id: If8f98eff64b3b8e56246829119814af8ef25d9c1

Remove SuperSU entry from Settings when SuperSU is not installed

* part of code taken from original "Launch SuperSU from Settings" commit by Jorrit Jongma <git@jongma.org>

Change-Id: I1f01c068395687ad8d6abd41f843553afdcd8a33

Add support for Phh Superuser to dashboard

- Instead of creating an entire new activity, just use one generic
  activity for all root management apps

- Generic # vector from materialdesignicons.com, see link for credit
https://materialdesignicons.com/icon/pound

Change-Id: I0572f7492fa56640db572fd0d4439ddbe40e8e95

Add support for Magisk to dashboard

Change-Id: Ia119cd7a696f129a4370372229f914018d8e1066
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

Android.mk
@@ -47,6 +47,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_FULL_LIBS_MANIFEST_FILES := $(LOCAL_PATH)/AndroidManifest-aex.xml
+
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
     --extra-packages android.support.v7.preference \
     --extra-packages android.support.v14.preference \

PureFusionOS/android_packages_apps_Settings

Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

Android.mk
@@ -39,6 +39,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_FULL_LIBS_MANIFEST_FILES := $(LOCAL_PATH)/AndroidManifest-fusion.xml
+
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
     LOCAL_JACK_ENABLED := incremental